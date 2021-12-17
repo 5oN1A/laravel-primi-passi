c4c9798a9c55e1fa82598da6deb0dde8b0fbde68
5oN1A <82112240+5oN1A@users.noreply.github.com>
2021-12-17 16:18:29 +0100

    route() links

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,9 +5,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>hello world</title>
+    <style>
+
+
+    </style>
 </head>
 <body>
     <h1>HELLO WORLD!</h1>
+    <header>
+        <h2>MVC Fundamentals:</h2>
+
+        <nav>
+            <ul>
+                <li><a href="{{ route('model') }}">Model</a></li>
+                <li><a href="{{ route('view') }}">View</a></li>
+                <li><a href="{{ route('controller') }}">Controller</a></li>
+            </ul>
+        </nav>
+    </header>
+    <hr>
 
     <h2>This page is made in {{ $name }} framework.</h2>
     <h3>There are other PHP frameworks as:</h3>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,40 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home', ['name' => 'Laravel', 'frameworks' =>['Code Igniter', 'Symfony', 'Yii Framework', 'Zend', 'FuelPhP'] ]);
-});
+    $data =  [
+        'name' => 'Laravel',
+        'frameworks' =>
+        [
+            'Code Igniter',
+            'Symfony',
+            'Yii Framework',
+            'Zend',
+            'FuelPhP'
+        ]
+    ];
+    return view('home', $data);
+})->name('Home');
+
+Route::get('/M', function () {
+    $data = [
+        'title' => 'M as model',
+        'text' => 'il model fornisce i metodi per accedere ai dati utili all\'applicazione.'
+    ];
+    return view('modello', $data);
+})->name('model');
+
+Route::get('/V', function () {
+    $data = [
+        'title' => 'V as view',
+        'text' => 'il view visualizza i dati contenuti nel model e si occupa dell\'interazione con utenti e agenti.'
+    ];
+    return view('vista',$data);
+})->name('view');
+
+Route::get('/C', function () {
+    $data = [
+        'title' => 'C as controller',
+        'text' => 'il controller riceve i comandi dell\'utente (in genere attraverso il view) e li attua modificando lo stato degli altri due componenti.'
+    ];
+    return view('controllo', $data);
+})->name('controller');
